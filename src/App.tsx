6c8f710e3fc799151e4612839d899139afaa91de
roncalw <roncalw@hotmail.com>
2023-02-23 12:11:09 -0500

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { Container } from "react-bootstrap"
+import { About } from "./pages/About"
+import { Home } from "./pages/Home"
+import { Store } from "./pages/Store"
+import { Navbar } from "./components/Navbar"
+import { ShoppingCartProvider } from "./context/ShoppingCartContext"
+
+function App() {
+
+  return (
+    <ShoppingCartProvider>
+    <BrowserRouter>
+    <Navbar />
+    <Container className="mb-4">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/store" element={<Store />} />
+        <Route path="/About" element={<About />} />
+      </Routes>
+    </Container>
+    </BrowserRouter>
+    </ShoppingCartProvider>
+  )
+}
+
+export default App
